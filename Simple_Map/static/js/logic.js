eff13627acb51a95919a760818c5271821629437
hiep_pham <hiep_pham@hiep-phams-MacBook-Pro-2.local>
2021-02-22 11:45:36 -0800

    Mapping Lines

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -15,6 +15,27 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
         maxZoom: 18,
         accessToken: API_KEY
     });
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+  console.log(city)
+ });
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, { 
+    radius: city.population/100000,
+    color: "Orange",
+    fillcolor: "#ffffa1"
+
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
     
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
