ee2fa498907548387f174e815ac2d562e9b3cf4f
hiep_pham <hiep_pham@hiep-phams-MacBook-Pro-2.local>
2021-02-22 17:29:41 -0800

    Map GeoJSON Polygons

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,20 +1,51 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-/// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
-
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
     });
+
+ // create another tile layer for satelliteStreets
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/satellite-streets-v11',
+  accessToken: API_KEY
+
+    });
+
+// create baseMaps
+let baseMaps = {
+  "Streets": streets,
+  "satellite and Streets": satelliteStreets
+};
+
+/// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center:[43.7, -79.3],
+            zoom: 11,
+  layers:[streets]
+});
+
+ // Pass our map layers into our layers control and add the layers control to the map.
+ L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/hieppham8083/Mapping_Earthquakes/main/torontoNeighborhoods.json";
     
-    // Then we add our 'graymap' tile layer to the map.
-    streets.addTo(map);
+// turn each feature into a marker on the map using pointToLayer
+d3.json(torontoHoods).then((data) =>{
+  L.geoJSON(data,{
+      // add style on lineStrings
+      style:{
+          "color":"blue",
+          "fillColor":"yellow",
+          "weight":2 },
+      onEachFeature: function (feature, layer) {
+          return layer.bindPopup("<h2> Neighborhood: " +feature.properties.AREA_NAME+"</h2>")
+      }
+  }).addTo(map);
+})
